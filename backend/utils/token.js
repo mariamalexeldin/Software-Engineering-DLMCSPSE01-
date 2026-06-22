6ddd0716a50bde7c1adc4081bb26d167e35f41eb
mariamalexeldin <mariamalexeldin@gmail.com>
2026-06-22 16:37:33 +0500

    Delete backend/utils directory

--- backend/utils/token.js
+++
@@ -1,8 +0,0 @@
-import jwt from "jsonwebtoken";
-
-export function createToken(userId) {
-  return jwt.sign({ id: userId }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN || "7d"
-  });
-}
-
